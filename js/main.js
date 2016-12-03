fab6d10f1e269770a049716a02ba86189ebf68b5
Yanny Castrillon <yannyandres117@gmail.com>
2016-12-03 01:40:13 -0800

    Function of collition working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 var $gameBoard = $(".game-board");
 var $gameBoardH= $gameBoard.height();
-var game = {
-  player1 : { score:0,name:"Yanny"},
-  player2 : { score:0,name:"Philippe"}
-}
-var currentPlayer = game.player1;
+var currentPlayer=null;
 var currentTime=0;
+var arrRocks = [];
+function Game(){
+  var game = this;
+  game.numRocks = 1;
+  game.myShuttle = new Shuttle();
+  game.player1 = { score:0,name:"Yanny"};
+  game.player2 = { score:0,name:"Philippe"};
+
+  for (var i = 0; i < game.numRocks; i++) {
+    arrRocks.push(new Rock("Rocker",14,2));
+  }
+
+}
+
+function checkCollision($bullet) {
+  var $rocks = $(".rock");
+  for (var i = 0; i < $rocks.length; i++){
+    var x1 = $rocks.position().left;
+    var y1 = $rocks.position().top;
+    var h1 = $rocks.outerHeight(true);
+    var w1 = $rocks.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $bullet.position().left;
+    var y2 = $bullet.position().top;
+    var h2 = $bullet.outerHeight(true);
+    var w2 = $bullet.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (!(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2)){
+      for (var j = 0; j < $rocks.length; j++) {
+        console.log("Es TRUE. Rocks");
+        console.log($rocks);
+        console.log($rocks[j]);
+        if($rocks[j].style.top == y1){
+          $($rocks[j]).remove();
+        }
+      }
+      $bullet.remove();
+    }
+    // returns a truthy or falsey value
+    return !(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2);
+  }
+}
 
 //Constructor of Space Shuttle
 function Shuttle(){
@@ -33,7 +74,7 @@ function Shuttle(){
     if (evt.key == "f") {
       // creates div and appends the bullet to the gameBoard
       var bullet = new Bullet(shuttle)
-      $gameBoard.append(bullet.node);
+
     }
   });
 
@@ -53,11 +94,13 @@ function Bullet(shuttle){
     top:bullet.y,
     left:bullet.x
   })
+  $gameBoard.append(bullet.node);
   bullet.node.animate({
     top: "-=" + bullet.vy
   },5,moveBulletY)
 
   function moveBulletY(){
+    checkCollision(bullet.node);
     // validates if bullet is out of the top of the game board
     if ($(bullet.node).offset().top < 0) {
       bullet.node.remove();
@@ -66,6 +109,7 @@ function Bullet(shuttle){
     $(bullet.node).animate({
       top: '-=' + bullet.vy
     },100,moveBulletY)
+
   }
 }
 
@@ -88,7 +132,7 @@ function Rock(name,size,points){
   rock.points=points;
   rock.vy = 20;
   //Starting position
-  rock.posX = genRandomNum(0,0);
+  rock.posX = genRandomNum(0,10);
   rock.posY = genRandomNum(0,100);
   rock.color=genRandomColor();
 
@@ -105,6 +149,7 @@ function Rock(name,size,points){
 
   // Moves the rock from one place to another
   function moveRock(){
+
     // Validates the position of each (div) rock to change value
     if (rock.node.offset().top > $gameBoardH) {
         rock.node.css({top:0})
@@ -117,21 +162,10 @@ function Rock(name,size,points){
     if(rock.vy == $gameBoardH) {
       rock.vy=0
     }
+    // checkCollision();
   }
 }
-
-var myShuttle = new Shuttle();
-
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
-new Rock("Rocker",14,2);
+new Game();
 
 
 // Get a random number between min and max
